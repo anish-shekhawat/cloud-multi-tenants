504464e018462e591875e49fab3a0e73a37e4675
anish-shekhawat <anish.s.shekhawat@gmail.com>
2017-04-01 20:00:06 -0700

    Adds simple login page

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
 app = Flask(__name__)
  
 @app.route("/")
-def main():
-        return "Welcome"
- 
+def index():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return index()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return render_template('login.html')
+
 if __name__ == "__main__":
-        app.run()
+	app.secret_key = os.urandom(12)
+	app.run()
