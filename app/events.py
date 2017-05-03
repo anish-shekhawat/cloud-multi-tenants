544c245e620227ab4218f76551d1db6fb486b731
Anish <anish.s.shekhawat@gmail.com>
2017-05-03 07:02:59 -0700

    Adds events for websockets

---
+++ app/events.py
@@ -0,0 +1,17 @@
+from flask import session
+from flask_socketio import emit, join_room, leave_room
+from app import socketio
+from flask.ext.login import current_user
+
+@socketio.on('connect')
+def invited_connect():
+    user_room = 'user_' + current_user.username
+    # Join a custom user room
+    join_room(user_room)
+    #Emit a connected status message
+    emit('status', {'data': 'Connected'})
+    print('Client Conencted')
+
+@socketio.on('disconnect')
+def invited_disconnect():
+    print('Client disconnected')
